0bef0e58512af8b431787d3db6aba7b2dc6c7050
Sean Durham <sean@sdurham.net>
2024-07-11 17:55:21 -0700

    TextArea takes up available vertical space

--- app/components/SpiralControls.js
+++ app/components/SpiralControls.js
@@ -25,7 +25,7 @@ function SpiralControls({
   }
 
   return (
-    <>
+    <div className="flex flex-col h-full pb-12">
       <h1 className="font-bold block">LED spiral calc</h1>
       <h2 className="font-bold mt-6 block">Parameters</h2>
       <label className="mt-4 block">
@@ -34,7 +34,7 @@ function SpiralControls({
           <input className="inline appearance-none" type="number" value={desiredDiameter} onChange={handleDiameterChange} />
           <span className="ml-1"> mm</span>
         </div>
-        <p className="mt-1">{mmToFt(desiredDiameter)}ft</p>
+        <p className="mt-1 text-slate-500">{mmToFt(desiredDiameter)}ft</p>
       </label>
       <label className="mt-2 block">
         Spacing between each LED
@@ -44,7 +44,7 @@ function SpiralControls({
           <option value={150}>150mm</option>
           <option value={200}>200mm</option>
         </select>
-        <p className="mt-1">{mmToIn(desiredSpacing)}in</p>
+        <p className="mt-1 text-slate-500">{mmToIn(desiredSpacing)}in</p>
       </label>
 
       <h2 className="mt-6 font-bold">Strip length</h2>
@@ -55,12 +55,12 @@ function SpiralControls({
       <p>{spiralLedCount}</p>
 
       <h2 className="font-bold mt-6">Points</h2>
-      <textarea className="mt-1 w-full h-24 p-2 rounded-md" readOnly>
+      <textarea className="mt-1 w-full h-24 p-2 rounded-md h-full min-h-24" readOnly>
         {spiralPoints.reduce((acc, point) => (
-          acc += `[${point[0].toFixed(3)}, ${point[1].toFixed(3)}], `
+        acc += `[${point[0].toFixed(3)}, ${point[1].toFixed(3)}], `
         ), "")}
       </textarea>
-    </>
+    </div>
   );
 }
 
